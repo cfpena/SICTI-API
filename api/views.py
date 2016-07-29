e348dff3d6251b13e5952046ffc3051da7f39ca4
cristian <cfpena@espol.edu.ec>
2016-07-29 00:51:31 -0500

    autenticacion por token

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from tokenapi.decorators import token_required
+from tokenapi.http import JsonResponse, JsonError
 from .serializers import UserSerializer, GroupSerializer
 
-
+@token_required
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -11,7 +13,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
-
+@token_required
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
