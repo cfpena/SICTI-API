005027dfcc5ddf75f3b0775d605f1f326d5a20a6
JaninaCosta <jany_costa90@hotamil.com>
2016-07-30 03:10:41 -0500

    validaciones de modulos item persona ..etc

--- api/models.py
+++ api/models.py
@@ -1,6 +1,13 @@
 from __future__ import unicode_literals
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.contrib.auth.models import User
 from django.db import models
+
+
+#validaciones
+solo_letras = RegexValidator(r'^[a-zA-Z]*$','Solo letras')
+solo_numeros = RegexValidator(r'^\d{1,10}$','Solo numeros')
+alfanumericos = RegexValidator(r'^[0-9a-zA-Z]*$','Solo alfanumericos')
 
 # Create your models here.
 class Personas(models.Model):
@@ -8,10 +15,10 @@ class Personas(models.Model):
         ('M', 'Masculino'),
         ('F', 'Femenino')
     )
-    CI = models.CharField(max_length=10,unique=True)
-    Nombre = models.CharField(max_length=30)
-    Apellido = models.CharField(max_length=30)
-    Email = models.EmailField(max_length=60,unique=True)
+    CI = models.CharField(max_length=10, unique=True, validators=[solo_numeros])
+    Nombre = models.CharField(max_length=30, validators=[solo_letras])
+    Apellido = models.CharField(max_length=30, validators=[solo_letras])
+    Email = models.EmailField(max_length=60, unique=True)
     Telefono = models.CharField(max_length=10,blank=True)
     Genero = models.CharField(
         max_length=2,
@@ -21,36 +28,37 @@ class Personas(models.Model):
     usuario = models.ForeignKey(User,null=True)
 
 class Items(models.Model):
-    Codigo = models.CharField(max_length=10,unique=True)
-    Nombre = models.CharField(max_length=30)
-    Marca = models.CharField(max_length=20)
-    Modelo = models.EmailField(max_length=20)
+    Codigo = models.CharField(max_length=10,unique=True, validators=[alfanumericos])
+    Nombre = models.CharField(max_length=20, validators=[alfanumericos])
+    Marca = models.CharField(max_length=20, blank=True, validators=[alfanumericos])
+    Modelo = models.CharField(max_length=20, blank=True, validators=[alfanumericos])
     Is_dispositivo = models.BooleanField(default=False)
     Is_kit = models.BooleanField(default=False)
-    Stock = models.IntegerField(default=0)
+    Stock = models.IntegerField(default=0, validators=[MaxValueValidator(50),MinValueValidator(1)])
+    Images = models.ImageField(upload_to='fotos', blank=True)
 
 class Items_Detalle_Kits(models.Model):
-    Cantidad = models.IntegerField(default=0)
+    Cantidad = models.IntegerField(default=0, validators=[MaxValueValidator(50),MinValueValidator(1)])
     fk_item = models.ForeignKey(Items,null=True)
 
 class Tipo_Identificacion(models.Model):
-    Nombre = models.CharField(max_length=30)
+    Nombre = models.CharField(max_length=20, validators=[alfanumericos])
 
 class Item_Identicacion(models.Model):
-    Valor = models.CharField(max_length=30)
+    Valor = models.CharField(max_length=20, validators=[alfanumericos])
     fk_item = models.ForeignKey(Items, null=True)
     fk_tipoIdentifacion = models.ForeignKey(Tipo_Identificacion, null=True)
 
 class Tipo_Movimiento(models.Model):
-    Nombre = models.CharField(max_length=30)
+    Nombre = models.CharField(max_length=20, validators=[alfanumericos])
 
 class Movimientos(models.Model):
     Fecha = models.DateField(auto_now=True)
     fk_tipoMovimiento = models.ForeignKey(Tipo_Movimiento, null=True)
 
 class Movimiento_Detalle(models.Model):
-    Cantidad = models.IntegerField(default=0)
-    Detalle = models.CharField(max_length=30)
+    Cantidad = models.IntegerField(default=0, validators=[MaxValueValidator(50),MinValueValidator(1)])
+    Detalle = models.CharField(max_length=30, validators=[alfanumericos])
     fk_item = models.ForeignKey(Items, null=True)
     fk_movimiento = models.ForeignKey(Movimientos,null=True)
 
@@ -60,10 +68,11 @@ class Movimiento_Detalle_ID(models.Model):
     fk_tipoIdenticacion = models.ForeignKey(Tipo_Identificacion, null=True)
 
 class Prestarios(models.Model):
-    Funcion = models.CharField(max_length=30)
+    Funcion = models.CharField(max_length=20 , validators=[alfanumericos])
     Activo = models.BooleanField(default=False)
     fk_persona = models.ForeignKey(Personas,null=True)
 
+#falta validar
 class Prestamos(models.Model):
     Fecha_vencimiento = models.DateField()
     Fecha_devolucion = models.DateField()
