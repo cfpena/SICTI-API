a7f530ae54cd19235fd805cb44f39e349c97401a
cristian <cfpena@espol.edu.ec>
2016-08-20 18:59:48 -0500

    cambio de tiempo de token

--- sistemaInventarioCTI/settings.py
+++ sistemaInventarioCTI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,6 +130,11 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 
     )
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': timedelta(days=2),
+    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
